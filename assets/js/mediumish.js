634f7edf8eb0bbcceef86bf520aa47ecd92e4165
Chris Hammond <github@christoc.com>
2024-02-02 08:31:20 -0800

    Updates to mediumish.js again

--- assets/js/mediumish.js
+++ assets/js/mediumish.js
@@ -37,15 +37,15 @@ jQuery(document).ready(function($){
         if (location.hash) {
           /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
           // window.scrollTo(0, 0);
-          target = location.hash.split('#');
-          smoothScrollTo($('#'+target[1]));
+          // target = location.hash.split('#');
+          // smoothScrollTo($('#'+target[1]));
         }
       }, 1);
 
       // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
       $('a[href*=\\#]:not([href=\\#])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          smoothScrollTo($(this.hash));
+          //smoothScrollTo($(this.hash));
           return false;
         }
       });
@@ -54,9 +54,9 @@ jQuery(document).ready(function($){
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
         if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
+          // $('html,body').animate({
+            // scrollTop: target.offset().top
+          // }, 1000);
         }
       }
     });
