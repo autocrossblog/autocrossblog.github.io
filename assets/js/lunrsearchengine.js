0022ae25b718df61e8b11d15ec85e760577f6378
ChrisHammond <ChrisHammond@users.noreply.github.com>
2024-07-12 16:11:04 +0000

    deploy: c33cdea945a584bb66e72d72f49078f8a3a1c528

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -403,7 +403,7 @@ var documents = [{
     "id": 80,
     "url": "https://www.autocrossblog.com/chris-hammond",
     "title": "The Intersection of Tech and Community: Chris Hammond's Journey",
-    "body": "2023/06/12 - A little about the owner here:: Balancing professional pursuits with community involvement isn’t easy, but Chris Hammond seems to have found the sweet spot. Based in St. Louis, MO, Chris has made significant contributions to his local community, all while advancing his career in the tech industry. Find out more about Chris’ inspiring journey on his personal blog, ChrisHammond. com. "
+    "body": "2023/06/12 - A little about the owner here:: Balancing professional pursuits with community involvement isn’t easy, but Chris Hammond seems to have found the sweet spot. Based in St. Louis, MO, Chris has made significant contributions to his local community, all while advancing his career in the tech industry. Find out more about Chris’ inspiring journey on his personal blog. Chris has owned far too many cars over the years and this website documents a number of those:  1973 VW Super Beetle 1984 Chevrolet Cavalier (won for graduation in 1995) 1994 VW Golf III 1999 Chevrolet Camaro Z-28 2003 Nissan 350z Enthusiast 1995 Suzuki GSXR 750 1988 Ford Thunderbird Turbo Coupe 1973 Datsun 240z 1978 Datsun 280z 2+2 2005 Chevrolet Tahoe Z71 2005 GMC Envoy 2004 Corvette Z06 2008 Ford F250 FX4 1995 Audi A6 2004 Nissan 350z Enthusiast 2010 Mazda CX-7 2011 Nissan Leaf 2008 Tahoe LS Z71 2013 Nissan Altima SV 2015 Nissan Altima SV 2004 Nissan 350z Touring 2017 Kia Sedona 2016 Scion FR-S 2022 Ford F250 1975 Triumph TR-6 (Dad’s) 2015 Ford Explorer (Dad’s) 2008 Chevrolet Corvette (Dad’s) 2022 Nissan Pathfinder"
     }, {
     "id": 81,
     "url": "https://www.autocrossblog.com/20-years-ago-today",
@@ -2042,7 +2042,7 @@ var documents = [{
     }, {
     "id": 408,
     "url": "https://www.autocrossblog.com/prepping-for-pulling-the-motor-out-of-the-datsun-240z",
-    "title": "Prepping for pulling the motor out of the Datsun 240z",
+    "title": "Prep to pull the motor out of the Datsun 240z",
     "body": "2007/01/29 - Spent some time today getting the car prepped for pulling the motor. Still a bit of work left to do. This video shows you some of the preparation work I had to do to get the motor out of the 240z for the first time. This video was early on in our Project240z days experienced inside the smaller garage of our rental house in Manchester Missouri. "
     }, {
     "id": 409,
